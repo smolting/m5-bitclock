12f2d338af9533361619533bd62b3145a561bc76
smolting <97995012+smolting@users.noreply.github.com>
2022-01-20 16:41:55 +0000

    Delete WifiConfiguration.h

--- WifiConfiguration.h
+++
@@ -1,7 +0,0 @@
-typedef struct {
-  char ssid[33];
-  char password[64];
-} WifiConfiguration;
-
-const WifiConfiguration homeWifi = {"rixel", "pumpitup"};
-const WifiConfiguration hotspot = {"flibbity", "bibbity"};
