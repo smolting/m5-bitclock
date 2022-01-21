8716deb9bad03ad77196ce5668a83921e3ce22ee
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2022-01-21 21:52:43 +0100

    move to esp_deep_sleep and turn off external green led

--- m5-bitclock.ino
+++ m5-bitclock.ino
@@ -312,8 +312,10 @@ void setup() {
 
   retrieveMetrics();
   InkPageSprite.pushSprite();
-
-  M5.shutdown(600);
+  
+  digitalWrite(LED_EXT_PIN, HIGH);
+  esp_sleep_enable_timer_wakeup(600 * 1000000);
+  esp_deep_sleep_start();
 }
 
 void loop() {}
